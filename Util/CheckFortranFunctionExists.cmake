136fb41e4652de74a89fa25a3594b5bd7a1c62bc
alinm.elena <alinm.elena@95cee11a-6a3d-0410-b919-3bd349e7ff26>
2008-07-22 15:13:30 +0000

    BLAS?LAPACK modules updated

--- Util/CheckFortranFunctionExists.cmake
+++ Util/CheckFortranFunctionExists.cmake
@@ -8,7 +8,6 @@
 # modify the way the check is run:
 #
 #  CMAKE_REQUIRED_LIBRARIES = list of libraries to link
-#
 
 macro(CHECK_FORTRAN_FUNCTION_EXISTS FUNCTION VARIABLE)
   if(NOT DEFINED ${VARIABLE})
@@ -19,14 +18,13 @@ macro(CHECK_FORTRAN_FUNCTION_EXISTS FUNCTION VARIABLE)
     else(CMAKE_REQUIRED_LIBRARIES)
       set(CHECK_FUNCTION_EXISTS_ADD_LIBRARIES)
     endif(CMAKE_REQUIRED_LIBRARIES)
-#    message(STATUS ${CHECK_FUNCTION_EXISTS_ADD_LIBRARIES})
-#    message(STATUS ${VARIABLE})
     FILE(WRITE
     ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/testFortranCompiler.f
     "
       program TESTFortran
       external ${FUNCTION}
-      end
+      call ${FUNCTION}()
+      end program TESTFortran
     "
     )
     try_compile(${VARIABLE}
@@ -35,6 +33,7 @@ macro(CHECK_FORTRAN_FUNCTION_EXISTS FUNCTION VARIABLE)
     CMAKE_FLAGS "${CHECK_FUNCTION_EXISTS_ADD_LIBRARIES}"
     OUTPUT_VARIABLE OUTPUT
     )
+#    message(STATUS "${OUTPUT}")
     if(${VARIABLE})
       set(${VARIABLE} 1 CACHE INTERNAL "Have Fortran function ${FUNCTION}")
       message(STATUS "Looking for Fortran ${FUNCTION} - found")
