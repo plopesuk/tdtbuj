bfa6ec23ed5649dbfb65d842d7bb1934f99bf98a
alinm.elena <alinm.elena@95cee11a-6a3d-0410-b919-3bd349e7ff26>
2008-07-22 15:13:30 +0000

    BLAS?LAPACK modules updated

--- trunk/Util/FindMKLSOLVER.cmake
+++ trunk/Util/FindMKLSOLVER.cmake
@@ -10,21 +10,29 @@
 #    is found
 #  MKLSOLVER_LINKER_FLAGS - uncached list of required linker flags (excluding -l
 #    and -L).
-#  MKLSOLVER_LIBRARIES - uncached list of libraries (using full path name) to 
+#  MKLSOLVER_LIBRARIES - uncached list of libraries (using full path name) to
 #    link against to use MKLSOLVER
- 
+#  MKLSOLVER95_LIBRARIES - uncached list of libraries (using full path name) to
+#    link against to use MKLSOLVER95 interface
+#  BLA_STATIC  if set on this determines what kind of linkage we do (static)
+#  BLA_F95     if set on tries to find the f95 interfaces for BLAS/LAPACK
+#  BLA_VENDOR  if set checks only the specified vendor, if not set checks
+#     Intel generic
+#     List of vendors (BLA_VENDOR) valid in this module
+##  Intel10_64lp (mkl mkl_solver_lp64), Intel (generic intel, default if not set)
+
 include(CheckFortranFunctionExists)
 set(MKLSOLVER_FOUND FALSE)
 
-macro(Check_MKLSOLVER_Libraries LIBRARIES _prefix _name _flags _list _BLAS)
+macro(Check_MKLSOLVER_Libraries LIBRARIES _prefix _name _flags _list _lapack _threads)
 # This macro checks for the existence of the combination of fortran libraries
-# given by _list.  If the combination is found, this macro checks (using the 
+# given by _list.  If the combination is found, this macro checks (using the
 # Check_Fortran_Function_Exists macro) whether can link against that library
 # combination using the name of a routine given by _name using the linker
 # flags given by _flags.  If the combination of libraries is found and passes
 # the link test, LIBRARIES is set to the list of complete library paths that
 # have been found.  Otherwise, LIBRARIES is set to FALSE.
- 
+
 # N.B. _prefix is the prefix applied to the names of all cached variables that
 # are generated internally and marked advanced by this macro.
 
@@ -35,20 +43,29 @@ foreach(_library ${_list})
   set(_combined_name ${_combined_name}_${_library})
 
   if(_libraries_work)
-IF (WIN32)
+  IF (WIN32)
+   if(BLA_STATIC)
+      set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib;.dll")
+    endif(BLA_STATIC)
     find_library(${_prefix}_${_library}_LIBRARY
     NAMES ${_library}
-    PATHS ENV LIB 
+    PATHS ENV LIB
     )
 ENDIF (WIN32)
 
   if(APPLE)
+    if(BLA_STATIC)
+      set(CMAKE_FIND_LIBRARY_SUFFIXES ".a;.so;.dylib")
+    endif(BLA_STATIC)
     find_library(${_prefix}_${_library}_LIBRARY
     NAMES ${_library}
     PATHS /usr/local/lib /usr/lib /usr/local/lib64 /usr/lib64 ENV DYLD_LIBRARY_PATH
     )
     else(APPLE)
-        find_library(${_prefix}_${_library}_LIBRARY
+    if(BLA_STATIC)
+      set(CMAKE_FIND_LIBRARY_SUFFIXES ".a;.so")
+    endif(BLA_STATIC)
+    find_library(${_prefix}_${_library}_LIBRARY
     NAMES ${_library}
     PATHS /usr/local/lib /usr/lib /usr/local/lib64 /usr/lib64 ENV LD_LIBRARY_PATH
     )
@@ -62,7 +79,7 @@ endforeach(_library ${_list})
 
 if(_libraries_work)
   # Test this combination of libraries.
-  set(CMAKE_REQUIRED_LIBRARIES ${_flags} ${${LIBRARIES}} ${_BLAS})
+  set(CMAKE_REQUIRED_LIBRARIES ${_flags} ${${LIBRARIES}} ${_lapack} ${_threads})
   #message("DEBUG: CMAKE_REQUIRED_LIBRARIES = ${CMAKE_REQUIRED_LIBRARIES}")
   check_fortran_function_exists(${_name} ${_prefix}${_combined_name}_WORKS)
   set(CMAKE_REQUIRED_LIBRARIES)
@@ -71,87 +88,137 @@ if(_libraries_work)
   #message("DEBUG: ${LIBRARIES} = ${${LIBRARIES}}")
 endif(_libraries_work)
 
-if(NOT _libraries_work)
+if(_libraries_work)
+  set(${LIBRARIES} ${${LIBRARIES}} ${_lapack})
+else(_libraries_work)
   set(${LIBRARIES} FALSE)
-endif(NOT _libraries_work)
+endif(_libraries_work)
 
 endmacro(Check_MKLSOLVER_Libraries)
 
 
 set(MKLSOLVER_LINKER_FLAGS)
 set(MKLSOLVER_LIBRARIES)
+set(MKLSOLVER95_LIBRARIES)
 
 if(MKLSOLVER_FIND_QUIETLY OR NOT MKLSOLVER_FIND_REQUIRED)
-  find_package(BLAS)
   find_package(LAPACK)
+  find_package(Threads)
 else(MKLSOLVER_FIND_QUIETLY OR NOT MKLSOLVER_FIND_REQUIRED)
-  find_package(BLAS REQUIRED)
   find_package(LAPACK REQUIRED)
+  find_package(Threads REQUIRED)
 endif(MKLSOLVER_FIND_QUIETLY OR NOT MKLSOLVER_FIND_REQUIRED)
 
-if(BLAS_FOUND)
-  set(MKLSOLVER_LINKER_FLAGS ${BLAS_LINKER_FLAGS})
-
+if(LAPACK_FOUND)
+  set(MKLSOLVER_LINKER_FLAGS ${LAPACK_LINKER_FLAGS})
 #intel MKLSOLVER
+  if ($ENV{BLA_VENDOR} MATCHES ".+")
+    set(BLA_VENDOR $ENV{BLA_VENDOR})
+  else ($ENV{BLA_VENDOR} MATCHES ".+")
+    if(NOT BLA_VENDOR)
+      set(BLA_VENDOR "All")
+    endif(NOT BLA_VENDOR)
+  endif ($ENV{BLA_VENDOR} MATCHES ".+")
+  if (BLA_VENDOR STREQUAL "Intel10_64lp")
+    if(BLA_F95)
+      if ( NOT MKLSOLVER95_LIBRARIES )
+        check_MKLSOLVER_libraries(
+        MKLSOLVER95_LIBRARIES
+        MKLSOLVER
+        djacobi_solve
+        ""
+        "mkl_solver_lp64"
+        "${LAPACK95_LIBRARIES}"
+        "${CMAKE_THREAD_LIBS_INIT}"
+        )
+      endif (NOT MKLSOLVER95_LIBRARIES)
+    else(BLA_F95)
+      if ( NOT MKLSOLVER_LIBRARIES )
+        check_MKLSOLVER_libraries(
+        MKLSOLVER_LIBRARIES
+        MKLSOLVER
+        djacobi_solve
+        ""
+        "mkl_solver_lp64"
+        "${LAPACK_LIBRARIES}"
+        "${CMAKE_THREAD_LIBS_INIT}"
+        )
+      endif (NOT MKLSOLVER_LIBRARIES)
+    endif(BLA_F95)
+  else(BLA_VENDOR STREQUAL "Intel10_64lp")
+    if(BLA_F95)
+      if ( NOT MKLSOLVER95_LIBRARIES )
+        check_MKLSOLVER_libraries(
+        MKLSOLVER95_LIBRARIES
+        MKLSOLVER
+        djacobi_solve
+        ""
+        "mkl_solver"
+        "${LAPACK95_LIBRARIES}"
+        "${CMAKE_THREAD_LIBS_INIT}"
+        )
+      endif (NOT MKLSOLVER95_LIBRARIES)
+    else(BLA_F95)
+      if ( NOT MKLSOLVER_LIBRARIES )
+        check_MKLSOLVER_libraries(
+        MKLSOLVER_LIBRARIES
+        MKLSOLVER
+        djacobi_solve
+        ""
+        "mkl_solver"
+        "${LAPACK_LIBRARIES}"
+        "${CMAKE_THREAD_LIBS_INIT}"
+        )
+      endif (NOT MKLSOLVER_LIBRARIES)
+    endif(BLA_F95)
+  endif(BLA_VENDOR STREQUAL "Intel10_64lp")
+else(LAPACK_FOUND)
+  message(STATUS "MKLSOLVE requires LAPACK")
+endif(LAPACK_FOUND)
 
-IF (WIN32)
-if ( NOT MKLSOLVER_LIBRARIES )
-    check_MKLSOLVER_libraries(
-    MKLSOLVER_LIBRARIES
-    MKLSOLVER
-    djacobi_solve
-    ""
-    "mkl_solver"
-    "${BLAS_LIBRARIES};${LAPACK_LIBRARIES}"
-    )
-  endif (NOT MKLSOLVER_LIBRARIES)
-ELSE (WIN32)
-if ( NOT MKLSOLVER_LIBRARIES )
-    check_MKLSOLVER_libraries(
-    MKLSOLVER_LIBRARIES
-    MKLSOLVER
-    djacobi_solve
-    ""
-    "mkl_solver_lp64"
-    "${BLAS_LIBRARIES};${LAPACK_LIBRARIES}"
-    )
-  endif (NOT MKLSOLVER_LIBRARIES)
-if ( NOT MKLSOLVER_LIBRARIES )
-    check_MKLSOLVER_libraries(
-    MKLSOLVER_LIBRARIES
-    MKLSOLVER
-    djacobi_solve
-    ""
-    "mkl_solver"
-    "${BLAS_LIBRARIES};${LAPACK_LIBRARIES}"
-    )
-  endif (NOT MKLSOLVER_LIBRARIES)
-
-
-
-ENDIF (WIN32)
-else(BLAS_FOUND)
-  message(STATUS "MKLSOLVE Rrequires BLAS")
-endif(BLAS_FOUND)
-
-if(MKLSOLVER_LIBRARIES)
+if(BLA_F95)
+ if(MKLSOLVER95_LIBRARIES)
+  set(MKLSOLVER95_FOUND TRUE)
+ else(MKLSOLVER95_LIBRARIES)
+  set(MKLSOLVER95_FOUND FALSE)
+ endif(MKLSOLVER95_LIBRARIES)
+ if(NOT MKLSOLVER_FIND_QUIETLY)
+    if(MKLSOLVER95_FOUND)
+      message(STATUS "A library with MKLSOLVER95 API found.")
+    else(MKLSOLVER95_FOUND)
+      if(MKLSOLVER_FIND_REQUIRED)
+        message(FATAL_ERROR
+        "A required library with MKLSOLVER95 API not found. Please specify library location."
+        )
+      else(MKLSOLVER_FIND_REQUIRED)
+        message(STATUS
+        "A library with MKLSOLVER95 API not found. Please specify library location."
+        )
+      endif(MKLSOLVER_FIND_REQUIRED)
+    endif(MKLSOLVER95_FOUND)
+  endif(NOT MKLSOLVER_FIND_QUIETLY)
   set(MKLSOLVER_FOUND TRUE)
-else(MKLSOLVER_LIBRARIES)
-  set(MKLSOLVER_FOUND FALSE)
-endif(MKLSOLVER_LIBRARIES)
+  set(MKLSOLVER_LIBRARIES "${MKLSOLVER95_LIBRARIES}")
+else(BLA_F95)
+  if(MKLSOLVER_LIBRARIES)
+    set(MKLSOLVER_FOUND TRUE)
+  else(MKLSOLVER_LIBRARIES)
+    set(MKLSOLVER_FOUND FALSE)
+  endif(MKLSOLVER_LIBRARIES)
 
-if(NOT MKLSOLVER_FIND_QUIETLY)
-  if(MKLSOLVER_FOUND)
-    message(STATUS "A library with MKLSOLVER API found.")
-  else(MKLSOLVER_FOUND)
-    if(MKLSOLVER_FIND_REQUIRED)
-      message(FATAL_ERROR 
-      "A required library with MKLSOLVER API not found. Please specify library location."
-      )
-    else(MKLSOLVER_FIND_REQUIRED)
-      message(STATUS
-      "A library with MKLSOLVER API not found. Please specify library location."
-      )
-    endif(MKLSOLVER_FIND_REQUIRED)
-  endif(MKLSOLVER_FOUND)
-endif(NOT MKLSOLVER_FIND_QUIETLY)
+  if(NOT MKLSOLVER_FIND_QUIETLY)
+    if(MKLSOLVER_FOUND)
+      message(STATUS "A library with MKLSOLVER API found.")
+    else(MKLSOLVER_FOUND)
+      if(MKLSOLVER_FIND_REQUIRED)
+        message(FATAL_ERROR
+        "A required library with MKLSOLVER API not found. Please specify library location."
+        )
+      else(MKLSOLVER_FIND_REQUIRED)
+        message(STATUS
+        "A library with MKLSOLVER API not found. Please specify library location."
+        )
+      endif(MKLSOLVER_FIND_REQUIRED)
+    endif(MKLSOLVER_FOUND)
+  endif(NOT MKLSOLVER_FIND_QUIETLY)
+endif(BLA_F95)
