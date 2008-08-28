fb92bc47abe049ab787fdc2ad423f6fa968aac25
alinm.elena@gmail.com <alinm.elena@gmail.com@95cee11a-6a3d-0410-b919-3bd349e7ff26>
2008-08-28 22:35:59 +0000

    thesis style improved plus some extra info added

--- trunk/Util/BfromCxz/source/Magnetic.f90
+++ trunk/Util/BfromCxz/source/Magnetic.f90
@@ -11,7 +11,7 @@ subroutine computeB(cxz)
   character(len=k_mw), parameter :: myname="computeB"
   type(cxzType), intent(inout) :: cxz
 
-  real(k_pr) :: rc(1:3),r,sina,cosa,rot(1:3,1:3),rm(1:3),rcp(1:3),invrot(1:3,1:3),a(1:3),b(1:3),mag(1:3),la,gB(1:3)
+  real(k_pr) :: rc(1:3),r,sina,cosa,rot(1:3,1:3),rm(1:3),rcp(1:3),invrot(1:3,1:3),a(1:3),b(1:3),mag(1:3),xa,gB(1:3),length
   integer :: i,j,k,m
 
   do j=1,cxz%nframes
@@ -33,20 +33,23 @@ subroutine computeB(cxz)
      b(1)=cxz%frames(j)%x(i+1)
      b(2)=cxz%frames(j)%y(i+1)
      b(3)=cxz%frames(j)%z(i+1)
-     rm=(a+b)/2.0_k_pr
-     la=norm(a-b)/2.0_k_pr
-     r=norm(rm)
-     sina=rm(2)/r
-     cosa=rm(1)/r
+     length=norm(b-a)
+     sina=(a(1)-b(1))/length
+     cosa=(b(2)-a(2))/length
      call Buildrotz(rot,cosa,-sina)
      ! matrix rot S'=rotS
      call Buildrotz(invrot,cosa,sina)
      ! matrix invrot S=rotS'
      rcp=matmul(rot,rc)
-     call localB(mag,rcp,la,r,cxz%frames(j)%bondCurrent(i)*k_mju0/(4.0_k_pr*k_pi))
+     a=matmul(rot,a)
+     b=matmul(rot,b)
+     xa=a(2)
+     r=a(1)
+     call localB(mag,rcp,xa,length,r,cxz%frames(j)%bondCurrent(i)*k_mju0/(4.0_k_pr*k_pi))
      gB=gB+matmul(invrot,mag)
     enddo
     cxz%frames(j)%b=gb*k_B2tesla
+
   enddo
   end subroutine computeB
 
@@ -83,14 +86,14 @@ subroutine computeB(cxz)
 
   end subroutine Buildrotz
 
-  subroutine localB(b,r,a,xr,pre)
+  subroutine localB(b,r,a,length,xr,pre)
   character(len=*), parameter :: myname="localB"
   real(k_pr), intent(inout) :: b(:)
-  real(k_pr), intent(in) :: r(:),a,xr,pre
+  real(k_pr), intent(in) :: r(:),a,xr,pre,length
   real(k_pr):: d,h
 
     d=sqrt((r(1)-xr)*(r(1)-xr)+r(3)*r(3))
-    h=((r(2)+a)/sqrt(d*d+(r(2)+a)*(r(2)+a))-(r(2)-a)/sqrt(d*d+(r(2)-a)*(r(2)-a)))/(d*d)
+    h=((a+length-r(2))/sqrt(d*d+(a+length-r(2))*(a+length-r(2)))-(a-r(2))/sqrt(d*d+(a-r(2))*(a-r(2))))/(d*d)
     b(2) = 0.0_k_pr
     b(1) = pre*h*r(3)
     b(3) =-pre*h*(r(1)-xr)
